fd2be34948d081b38e611bd107e9b9d6885dce4e
2316hako <zp5knmepsu@outlook.jp>
2023-06-30 14:32:39 +0900

    検索機能完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,7 @@ class Item < ApplicationRecord
   belongs_to :condition
 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
 end
